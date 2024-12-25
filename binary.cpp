4c047a54a9145875efc2c981f95faf105ab22b8a
Yash Patil <yashvardhanpatil10@gmail.com>
2024-12-25 12:33:35 +0530

    Update binary.cpp

--- binary.cpp
+++ binary.cpp
@@ -1,43 +1,38 @@
+// binary exponeatiation
+//   *NOTE : ELEMENTS SHOULD BE SORTED*
 #include <iostream>
-#include <algorithm>
+#include <vector>
 using namespace std;
+int binary_expo(int target, vector<int> nums)
+{
+    int n = nums.size();
+    int start = 0, end = n - 1, mid;
+    while (start < end)
+    {
+        mid = start + (end - start) / 2;
+        if (target == nums[mid])
+        {
+            return mid;
+        }
+        else if (target < nums[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    return -1;
+};
 
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    vector<int> nums = {2, 3, 4, 6, 8, 9, 10, 22, 33, 45, 67, 89};
+    int target = 67;
+    int index = binary_expo(target, nums);
+    cout << "the target is at " << index << " index" << endl;
 
-    int target;
-    cin >> target;
-    sort(arr, arr + n);
-
-    int start = 0;
-    int end = n - 1;
-    int mid;
-
-    while (start <= end) {
-        mid = start + (end - start) / 2;
-
-        if (arr[mid] == target) {
-            cout << mid << endl;
-            return 0;
-        }
-        else if (arr[mid] < target) {
-            start = mid + 1;
-        }
-        else {
-            end = mid - 1;
-        }
-    }
-
-    cout << "Target not found" << endl;
     return 0;
 }
+
